34fb5f9036893595344d75b2a5ce3158109bc476
jwwasylenko <jwwasy@gmail.com>
2024-11-07 17:51:50 -0700

    Finished the Lab
    I know I still have lots to learn. Honestly been working pretty hard at this and it feels good to actually write some code down.

---
+++ Lab2.js
@@ -0,0 +1,29 @@
+elk.addEventListener('click',function (){
+    console.log("Animal being changed");
+    document.getElementById("banner").src="elk.webp";
+
+})
+
+moose.addEventListener('click',function (){
+    console.log("Animal being changed");
+    document.getElementById("banner").src="moose.webp";
+
+})
+
+sheep.addEventListener('click',function (){
+    console.log("Animal being changed");
+    document.getElementById("banner").src="sheep.webp";
+
+})
+
+wolf.addEventListener('click',function (){
+    console.log("Animal being changed");
+    document.getElementById("banner").src="wolf.webp";
+
+})
+
+bear.addEventListener('click',function (){
+    console.log("Animal being changed");
+    document.getElementById("banner").src="bear.webp";
+
+})
